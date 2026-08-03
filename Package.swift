45ac9d0b147c89a5ae27eae99dcd82b95ab8617a
Radek Serek <radoslaw.serek@hotmail.com>
2026-08-03 13:37:27 +0200

    Update Package.swift for 1.0.2-RC3

--- Package.swift
+++ Package.swift
@@ -1,21 +1,25 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// Template for the public HGSNS/HigoCore SPM distribution repository.
+// URL and checksum are updated by Fastlane release lanes.
 
 import PackageDescription
 
 let package = Package(
     name: "HigoCore",
+    platforms: [
+        .iOS(.v17)
+    ],
     products: [
         .library(
             name: "HigoCore",
             targets: ["HigoCore"]
-            ),
+        )
     ],
     targets: [
         .binaryTarget(
             name: "HigoCore",
-            url: "https://github.com/HGSNS/HigoCore/releases/download/1.0.1/HigoCore.xcframework.zip",
-            checksum: "205dab633814091cda7640d1673b5c517bf316f4ea67ececa5b185963bd88244"
+            url: "https://github.com/HGSNS/HigoCore/releases/download/1.0.2-RC3/HigoCore.xcframework.zip",
+            checksum: "f5ffc81966f537d6b209f191f449546fe96ad2d5262dc6cdd4b1f6016bb4db27"
         )
     ]
 )
